fafe6c65b4600441ebe5ad14cd69f7eaec67407b
Brenn Kucey <bkucey@newrelic.com>
2022-02-07 19:30:52 -0800

    add button

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("JavaScript loaded");
+let button = document.querySelector("#solve-it-button");
+button.onclick = (e) => {
+    alert("I'm working on it :) This is not implemented yet :/");
+}
