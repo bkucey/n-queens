311eeb3e0245c691e13d9c4a0628cc833c17d0b3
Brenn Kucey <bkucey@newrelic.com>
2022-02-07 18:52:08 -0800

    psuedo code for n queens problem

---
+++ n-queens.js
@@ -0,0 +1,10 @@
+// init board function 
+
+// addQueen(board, row, success) => {board, success}
+    //base case : if there are no more rows, then board is done! 
+
+    //try each column 
+        //if this spot works so far 
+            //make a copy of the board 
+            //send that to addQueen(board, row+1)
+    // none of the columns on this row worked. Return false;         
